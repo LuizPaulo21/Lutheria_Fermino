7f85c950b6f44e6aecbccc8a8ea29d91680b6257
Luiz Paulo <luiz.paulo.neto@hotmail.com>
2024-05-03 22:04:08 -0300

    Paginas de consulta e inclusão de pedidos criadas

--- app/routes.py
+++ app/routes.py
@@ -4,20 +4,20 @@ from flask import render_template
 from .bdcon import *
 import hashlib
 import dotenv
-import logging
 import os
 
 
 
 dotenv.load_dotenv()
 app.secret_key=os.getenv('SECRET_KEY')
+cnx = mysql_con()
 
 @app.route("/")
 @app.route("/index")
 def home():
     return render_template('index.html')
 
-
+# Criação de login e sessão
 @app.route("/login", methods=['POST'])
 def check_login():
     # Checa se o método é POST e se os campos usuario e senha estão preenchidos
@@ -32,7 +32,6 @@ def check_login():
         senha = hash.hexdigest()
 
         # Checa se a conta existe no banco de dados
-        cnx = mysql_con()
         cursor = cnx.cursor()
         cursor.execute('SELECT * FROM usuario WHERE usuario = %s AND senha = %s', (usuario, senha,))
 
@@ -52,8 +51,6 @@ def check_login():
             msg = "Usuario ou senha incorretos!"
 
     return render_template('index.html', msg=msg)
-
-
     
 #Tela de cadastro de novos clientes
 @app.route("/cadastrar.html")
@@ -62,3 +59,111 @@ def cadastrar():
         return render_template("cadastrar.html")
     else:
         return render_template("index.html", msg="Acesso negado, faça o login!")
+
+# Salva novo cliente no banco de dados    
+@app.route("/salvar", methods=['POST'])
+def salvar_cliente():
+    if session['loggedin'] == True:
+
+        # Busca os dados do formulario e salva nas variáveis
+        cpf  = request.form.get('cpf')
+        nome = request.form.get('nome')
+        endereco = request.form.get('endereco')
+        complemento = request.form.get('complemento')
+        bairro = request.form.get('bairro')
+        cidade = request.form.get('cidade')
+        estado = request.form.get('estado')
+        cep = request.form.get('cep')
+        telefone = request.form.get('telefone')
+        celular = request.form.get('celular')
+        contato = request.form.get('contato')
+        email = request.form.get('email')
+        observacao = request.form.get('observacao')
+
+        #criação cursor
+        cursor = cnx.cursor()
+        cursor.execute('SELECT * FROM cliente WHERE CPF = %s', (cpf,))     
+
+        # Busca um registro
+        registro = cursor.fetchone()   
+
+        # Se o registro existe
+        if registro:
+            return render_template('cadastrar.html', msg="Cliente já cadastrado") 
+        else:
+            #Insere primeiramente um enderço
+            cursor.execute('INSERT INTO endereco (cep, logradouro, bairro, cidade, estado, complemento) VALUES (%s, %s, %s, %s, %s, %s)', (cep, endereco, bairro, cidade, estado, complemento))
+            
+            #retorna o id do ultimo registro da tabela
+            ultimoid = cursor.lastrowid
+
+            #insere finalmente o cliente
+            cursor.execute('INSERT INTO cliente (CPF, nome, email, telefone, contato, celular, observacao, idendereco) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (cpf, nome, email, telefone, contato, celular, observacao, ultimoid))
+
+            # Insere as informações no banco de dados
+            cnx.commit()
+
+            # Retorna mensagem de sucesso no cadastro
+            return render_template("cadastrar.html", msg="Cliente cadastrado com sucesso!")
+
+# Buscar um cadastro
+@app.route("/buscar.html")
+def buscar():
+    if session['loggedin'] == True:
+        return render_template("buscar.html")
+    else:
+        return render_template("index.html", msg="Acesso negado, faça o login!")
+    
+# Busca um registro no banco de dados
+@app.route("/procurar", methods=['POST'])
+def procurar():
+
+    # Se usuario estiver logado
+    if session['loggedin'] == True:
+
+        cursor = cnx.cursor()
+
+        # Salva os dados do formulário
+        tipo = request.form.get('tipo')
+        dado = request.form.get('textobusca')
+
+        #Fazendo a busca de acordo com o dado
+        if tipo == 'CEP':
+
+            cursor.execute('SELECT * FROM endereco WHERE CEP = %s', (dado,))
+            registro = cursor.fetchone()
+
+
+            # Se o endereço estiver cadastrado
+            if registro:
+                ultimoid = cursor.lastrowid
+                # Buscar o cadastro que possui o CEP
+                cursor.execute('SELECT * FROM cliente WHERE idendereco = %s', (ultimoid,))
+
+                #Retorna os dados populados na página
+                return render_template("consultarres.html")
+
+        if tipo == 'CPF':
+            cursor.execute('SELECT * FROM cliente WHERE CPF = %s', (dado,))
+            registro = cursor.fetchone()
+            cursor.execute('SELECT * FROM endereco WHERE idendereco = %s', (registro[8],))
+            endereco = cursor.fetchone()
+            return render_template("consultarres.html", dados = registro, endereco = endereco)
+        
+        if tipo == 'NOME':
+            cursor.execute('SELECT * FROM cliente WHERE nome = %s', (dado,))
+            return render_template("consultarres.html")
+
+        else:
+            return render_template("buscar.html", msg="Cliente não consta na base de dados.")
+
+
+# Página para inserir um pedido
+@app.route("/pedido.html")
+def incluir_pedido():
+    return render_template("pedido.html")
+
+# Página para consultar um pedido
+@app.route("/consultarpedido.html")
+def consultar_pedido():
+    return render_template("consultarpedido.html")
